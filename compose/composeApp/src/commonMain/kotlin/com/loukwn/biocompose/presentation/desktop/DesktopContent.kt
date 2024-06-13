a7ea9a62961caeb278ab54f77691ae9b52b6cdbd
loukwn <main@loukwn.me>
2024-06-13 18:15:26 +0100

    feat: Some work on the click animations + added some hover effects

--- compose/composeApp/src/commonMain/kotlin/com/loukwn/biocompose/presentation/desktop/DesktopContent.kt
+++ compose/composeApp/src/commonMain/kotlin/com/loukwn/biocompose/presentation/desktop/DesktopContent.kt
@@ -2,26 +2,49 @@ package com.loukwn.biocompose.presentation.desktop
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.animation.slideInVertically
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.awaitFirstDown
+import androidx.compose.foundation.gestures.waitForUpOrCancellation
+import androidx.compose.foundation.hoverable
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.interaction.collectIsHoveredAsState
 import androidx.compose.foundation.layout.Arrangement.Absolute.spacedBy
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.foundation.lazy.staggeredgrid.items
-import androidx.compose.material.Text
-import androidx.compose.material.ripple.rememberRipple
-import androidx.compose.runtime.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.PointerIcon
+import androidx.compose.ui.input.pointer.pointerHoverIcon
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.loukwn.biocompose.presentation.design_system.components.HoverableText
 import kotlinx.coroutines.delay
 import loukwn_me_kotlin_wasm.composeapp.generated.resources.Res
 import loukwn_me_kotlin_wasm.composeapp.generated.resources.avatar
@@ -75,9 +98,13 @@ private fun DesktopItem(
     drawableResource: DrawableResource,
     title: String,
     animationStartDelay: Long,
+    modifier: Modifier = Modifier,
     onClick: () -> Unit = {}
 ) {
     var shown by remember { mutableStateOf(false) }
+    val hoverInteractionSource = remember { MutableInteractionSource() }
+    val isHovered by hoverInteractionSource.collectIsHoveredAsState()
+    val tint = if (isHovered) Color.Black.copy(alpha = .5f) else null
 
     LaunchedEffect(Unit) {
         delay(animationStartDelay)
@@ -94,22 +121,55 @@ private fun DesktopItem(
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .clickable(
-                    interactionSource = remember { MutableInteractionSource() },
-                    indication = rememberRipple(bounded = false),
-                    role = Role.Button,
-                    onClick = onClick,
-                )
+                .hoverable(hoverInteractionSource)
+                .pointerHoverIcon(PointerIcon.Hand)
+                .clickWithBounceEffect(onClick)
                 .padding(8.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = spacedBy(8.dp),
+            verticalArrangement = spacedBy(16.dp),
         ) {
             Image(
                 painter = painterResource(drawableResource),
                 contentDescription = title,
-                modifier = Modifier.size(48.dp),
+                modifier = modifier.size(56.dp).clip(CircleShape),
+                colorFilter = tint?.let { ColorFilter.tint(it, BlendMode.Multiply) }
             )
-            Text(title, color = Color.White, textAlign = TextAlign.Center)
+
+            HoverableText(
+                text = title,
+                textAlign = TextAlign.Center,
+                hoverInteraction = hoverInteractionSource,
+            )
         }
     }
+}
+
+private fun Modifier.clickWithBounceEffect(onClick: () -> Unit) = this then composed {
+    var buttonIsPressed by remember { mutableStateOf(false) }
+    val scale by animateFloatAsState(
+        targetValue = if (buttonIsPressed) 0.80f else 1f,
+        animationSpec = spring(Spring.DampingRatioMediumBouncy)
+    )
+
+    this
+        .graphicsLayer {
+            scaleX = scale
+            scaleY = scale
+        }.clickable(
+            interactionSource = remember { MutableInteractionSource() },
+            indication = null,
+            role = Role.Button,
+            onClick = onClick,
+        )
+        .pointerInput(buttonIsPressed) {
+            awaitPointerEventScope {
+                if (buttonIsPressed) {
+                    waitForUpOrCancellation()
+                } else {
+                    awaitFirstDown(false)
+                }
+
+                buttonIsPressed = !buttonIsPressed
+            }
+        }
 }
