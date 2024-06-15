ea6bf0dbe68efb723538a71b0aad0fe9ed044120
loukwn <main@loukwn.me>
2024-06-15 14:32:02 +0100

    feat: Added the avatar with blurry animation on AboutMe

--- compose/composeApp/src/commonMain/kotlin/com/loukwn/biocompose/presentation/aboutme/AboutMeContent.kt
+++ compose/composeApp/src/commonMain/kotlin/com/loukwn/biocompose/presentation/aboutme/AboutMeContent.kt
@@ -1,12 +1,28 @@
 package com.loukwn.biocompose.presentation.aboutme
 
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.delay
+import loukwn_me_kotlin_wasm.composeapp.generated.resources.Res
+import loukwn_me_kotlin_wasm.composeapp.generated.resources.me_grayscaled
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.painterResource
+
 
 @Composable
 fun AboutMeContent(component: AboutMeComponent, onSystemUiModeChanged: (isLight: Boolean) -> Unit) {
@@ -16,5 +32,25 @@ fun AboutMeContent(component: AboutMeComponent, onSystemUiModeChanged: (isLight:
 
     Box(Modifier.fillMaxSize(1f).background(Color(0xff34343f))) {
         AboutMeBg(modifier = Modifier.fillMaxSize())
+        Avatar()
     }
+}
+
+@OptIn(ExperimentalResourceApi::class)
+@Composable
+private fun Avatar() {
+    var targetBlurRadius by remember { mutableStateOf(25.dp) }
+    val blurRadiusAnimated by animateDpAsState(targetBlurRadius, tween(1000))
+
+    LaunchedEffect(Unit) {
+        delay(200)
+        targetBlurRadius = 0.dp
+    }
+
+    Image(
+        painterResource(Res.drawable.me_grayscaled),
+        "",
+        modifier = Modifier.fillMaxSize().blur(radiusX = blurRadiusAnimated, radiusY = blurRadiusAnimated),
+        contentScale = ContentScale.Crop
+    )
 }
