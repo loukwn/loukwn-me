f49ce3312ea99a2fff79a9755aa38554d0af6107
loukwn <main@loukwn.me>
2024-12-15 00:40:26 +0000

    chore: Added build script

---
+++ build.sh
@@ -0,0 +1,19 @@
+# Delete unneeded files
+find . -maxdepth 1 -type f -delete
+
+# Create CNAME
+touch CNAME && echo "loukwn.me" > CNAME
+
+# Move Web stuff to root folder
+mv web/* .
+rm -rf web
+
+# Build Compose
+cd compose
+./gradlew wasmJsBrowserDistribution
+cd ..
+mkdir compose2
+mv compose/composeApp/build/dist/wasmJs/productionExecutable/* compose2/
+rm -rf compose
+mv compose2/ compose
+
